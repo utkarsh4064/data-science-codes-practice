86c7a16af3d4746f3ad967f83632e91f01dababf
Utkarsh Dixit <utkarshdixitgkp74@gmail.com>
2022-09-20 20:49:24 +0530

    two methods depiction of json and python interaction

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ content= json.loads(s)
 print(content)
 print(content['aman'])
 print(content['aman']['age'])
+
+d={"india":{"captain":"rohit sharma", "wicket-keeper":"rishabh pant",
+            "coach":"ravi shahstri"}, "australia":{"captain": "pat cummins",
+            "wicket-keeper": "brad haddin", "coach": "garry kirsten"}}
+
+print(json.dumps(d['india']['captain']))
+print(json.dumps(d))
+print(json.dumps(d['australia']))
